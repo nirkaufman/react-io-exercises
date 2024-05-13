58dca47ef1edee9fd5a28aa97c5678871fb9fe63
nirkaufman <nirkaufman@gmail.com>
2024-05-13 15:11:20 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-6xl font-bold">React IO: 2024</h1>
+      </div>
+  )
+}
+
+export default App
